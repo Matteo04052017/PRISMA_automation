3cc418234d89b94bf6b628bea25c4966b51d755a
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2021-06-21 11:27:31 +0000

    check is calibrated
    Signed-off-by: Matteo Di Carlo <matteo.dicarlo@inaf.it>

--- calibrate.py
+++ calibrate.py
@@ -64,17 +64,38 @@ def getlast_n_days(last_n_days):
         day_capture_directories.append(date_to_consider.strftime("%Y%m%d"))
     return day_capture_directories
 
+def is_calibrated(camera_code, day):
+    # ITCP02_20210614_assoc.txt
+    # ITCP02_20210614_astro_covar.txt
+    # ITCP02_20210614_astro_error.txt
+    # ITCP02_20210614_astro_param.txt
+    # ITCP02_20210614_astro_report.pdf
+    # ITCP02_20210614_astro_sigma.txt
+    # ITCP02_20210614_astro_solution.txt
+    # ITCP02_20210614_photo_param.txt
+    # ITCP02_20210614_photo_report.pdf
+    # ITCP02_20210614_photo_sigma.txt
+    # ITCP02_20210614_photo_solution.txt
+    check_files = ["assoc.txt", "astro_covar.txt", "astro_error.txt", 
+    "astro_param.txt", "astro_report.pdf", "astro_sigma.txt", "astro_solution.txt",
+    "photo_param.txt", "photo_report.pdf", "photo_sigma.txt", "photo_solution.txt"]
+    for check_file in check_files:
+        filename = [camera_code,day,check_file]
+        local_file = "_".join(filename)
+        if not os.path.isfile(local_file): 
+            return False
+    return True
+    
+
 def calibrate_byday(day_capture_directories, camera_list):
     for c in camera_list:
         for d in day_capture_directories:
+            if is_calibrated(c,d):
+                continue
             cmd = ['bash', '-c', f'/usr/local/harris/idl/bin/idl -e "calibration, \'{c}\', \'{d}\', process_image=1, process_day=1, process_month=0, config_file=\'/astrometry/workspace/settings/configuration.ini\'"']
             separator = " "
             logger.info(separator.join(cmd))
-            cp = subprocess.run(cmd, universal_newlines=True)
-            if cp.returncode != 0:
-                logger.error("subprocess stderr %s", cp.stderr)
-            else:
-                logger.info("subprocess stdout %s", cp.stdout)
+            subprocess.run(cmd, universal_newlines=True)
 
 def main_loop():
     while True:
