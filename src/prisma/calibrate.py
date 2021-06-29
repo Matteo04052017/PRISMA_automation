0ba1715e91f3f8b028116778c367a66a8b0a6a81
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2021-06-29 16:05:42 +0200

    fix
    Signed-off-by: Matteo Di Carlo <matteo.dicarlo@inaf.it>

--- src/prisma/calibrate.py
+++ src/prisma/calibrate.py
@@ -83,7 +83,9 @@ def main_loop():
 
         logger.info(camera_codes)
 
-        calibrate_byday(get_days_to_work(last_n_days), camera_codes)
+        res = get_days_to_work(last_n_days)
+
+        calibrate_byday(res.day_capture_directories, camera_codes)
 
         logger.info("Stop execution at %s", st)
         time.sleep(SLEEP_TIME)
