540e0fc9c95fc8a3e08fd8d3f6b7c9f83ab58aec
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2021-07-06 16:36:48 +0200

    add montly calibration and fixes
    Signed-off-by: Matteo Di Carlo <matteo.dicarlo@inaf.it>

--- src/prisma/calibrate.py
+++ src/prisma/calibrate.py
@@ -16,35 +16,56 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 logger.addHandler(ch)
 
+# ITCP02_20210614_assoc.txt
+# ITCP02_20210614_astro_covar.txt
+# ITCP02_20210614_astro_error.txt
+# ITCP02_20210614_astro_param.txt
+# ITCP02_20210614_astro_report.pdf
+# ITCP02_20210614_astro_sigma.txt
+# ITCP02_20210614_astro_solution.txt
+# ITCP02_20210614_photo_param.txt
+# ITCP02_20210614_photo_report.pdf
+# ITCP02_20210614_photo_sigma.txt
+# ITCP02_20210614_photo_solution.txt
+check_files = [
+    "assoc.txt",
+    "astro_covar.txt",
+    "astro_error.txt",
+    "astro_param.txt",
+    "astro_report.pdf",
+    "astro_sigma.txt",
+    "astro_solution.txt",
+    "photo_param.txt",
+    "photo_report.pdf",
+    "photo_sigma.txt",
+    "photo_solution.txt",
+]
+
 last_n_days = 30
 errors = []
 
 
+def leap_year(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
+
+
+def days_in_month(month, year):
+    if month in {1, 3, 5, 7, 8, 10, 12}:
+        return 31
+    if month == 2:
+        if leap_year(year):
+            return 29
+        return 28
+    return 30
+
+
 def is_calibrated(camera_code, day):
-    # ITCP02_20210614_assoc.txt
-    # ITCP02_20210614_astro_covar.txt
-    # ITCP02_20210614_astro_error.txt
-    # ITCP02_20210614_astro_param.txt
-    # ITCP02_20210614_astro_report.pdf
-    # ITCP02_20210614_astro_sigma.txt
-    # ITCP02_20210614_astro_solution.txt
-    # ITCP02_20210614_photo_param.txt
-    # ITCP02_20210614_photo_report.pdf
-    # ITCP02_20210614_photo_sigma.txt
-    # ITCP02_20210614_photo_solution.txt
-    check_files = [
-        "assoc.txt",
-        "astro_covar.txt",
-        "astro_error.txt",
-        "astro_param.txt",
-        "astro_report.pdf",
-        "astro_sigma.txt",
-        "astro_solution.txt",
-        "photo_param.txt",
-        "photo_report.pdf",
-        "photo_sigma.txt",
-        "photo_solution.txt",
-    ]
     for check_file in check_files:
         filename = [camera_code, day, check_file]
         logger.info("filename %s", filename)
@@ -52,6 +73,17 @@ def is_calibrated(camera_code, day):
         if not os.path.isfile(
             "/astrometry/workspace/astrometry/" + local_file
         ):
+            return False
+    return True
+
+
+def is_month_complete(camera_code, month_str):
+    month = int(month_str[4:6])
+    year = int(month_str[0:4])
+    days = days_in_month(month, year)
+    for x in range(days):
+        mydate = datetime.date(year, month, x + 1)
+        if not is_calibrated(camera_code, mydate.strftime("%Y%m%d")):
             return False
     return True
 
@@ -80,6 +112,30 @@ def calibrate_byday(day_capture_directories, camera_list):
                 errors.append("calibration error for (%s, %s)", c, d)
 
 
+def calibrate_bymonth(month_capture_directories, camera_list):
+    for c in camera_list:
+        for m in month_capture_directories:
+            error_str = "(%s, %s)", c, m
+            if error_str in errors:
+                continue
+            if is_month_complete(c, m):
+                logger.info("is_month_complete(%s, %s) True", c, m)
+                continue
+            cmd = [
+                "bash",
+                "-c",
+                f"/usr/local/harris/idl/bin/idl -e \"calibration, '{c}', '{m}', process_image=0, process_day=0, process_month=1, config_file='/astrometry/workspace/settings/configuration.ini'\"",  # noqa: E501
+            ]
+            separator = " "
+            logger.info(separator.join(cmd))
+            try:
+                subprocess.run(cmd, universal_newlines=True, check=True)
+            except Exception as ex:
+                logger.error("%s", ex)
+                errors.append(error_str)
+                errors.append("calibration error for (%s, %s)", c, m)
+
+
 def main_loop():
     while True:
         st = datetime.datetime.fromtimestamp(time.time()).strftime(
@@ -96,6 +152,8 @@ def main_loop():
         day_capture_directories, month_capture_directories = get_days_to_work(
             last_n_days
         )
+
+        calibrate_bymonth(month_capture_directories, camera_codes)
 
         calibrate_byday(day_capture_directories, camera_codes)
 
---
+++ tests/unit/test_calibrate.py
@@ -0,0 +1,15 @@
+import datetime
+from prisma.calibrate import days_in_month
+
+
+def test_day_in_month():
+    month_str = "202106"
+    month = int(month_str[4:6])
+    year = int(month_str[0:4])
+    days = days_in_month(month, year)
+    res_days = []
+    for x in range(days):
+        mydate = datetime.date(year, month, x + 1)
+        res_days.append(mydate.strftime("%Y%m%d"))
+    assert "20210601" in res_days
+    assert "20210630" in res_days
